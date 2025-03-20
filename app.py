090025e3aac7fcd36a6e670e07096d1cda4d1967
Luckas Fonck <luckas.fonck@gmail.com>
2025-03-20 12:42:38 +0100

    Front end

---
+++ app.py
@@ -0,0 +1,55 @@
+import streamlit as st
+from PIL import Image
+import requests
+from dotenv import load_dotenv
+import os
+
+# Set page tab display
+st.set_page_config(
+   page_title="Simple Image Uploader",
+   page_icon= '🖼️',
+   layout="wide",
+   initial_sidebar_state="expanded",
+)
+
+# Example local Docker container URL
+# url = 'http://api:8000'
+# Example localhost development URL
+# url = 'http://localhost:8000'
+load_dotenv()
+url = os.getenv('API_URL')
+
+
+# App title and description
+st.header('Time to classify your trash! 📸')
+st.markdown(''' Some nice text will be added here in the future
+            ''')
+
+st.markdown("---")
+
+### Create a native Streamlit file upload input
+st.markdown("### Give me some trash!👇")
+img_file_buffer = st.file_uploader('Upload an image')
+
+if img_file_buffer is not None:
+
+  col1, col2 = st.columns(2)
+
+  with col1:
+    ### Display the image user uploaded
+    st.image(Image.open(img_file_buffer), caption="Your lovely peace of watse ☝️")
+
+  with col2:
+    with st.spinner("Wait for it..."):
+      ### Get bytes from the file buffer
+      img_bytes = img_file_buffer.getvalue()
+
+      ### Make request to  API (stream=True to stream response as bytes)
+      res = requests.post(url + "/upload_image", files={'img': img_bytes})
+
+      if res.status_code == 200:
+        ### Display the image returned by the API
+        st.image(res.content, caption="Image returned from API ☝️")
+      else:
+        st.markdown("**Oops**, something went wrong 😓 Please try again.")
+        print(res.status_code, res.content)
